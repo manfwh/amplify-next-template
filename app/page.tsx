b540f3c0b9b4bbbbd20433a780fd25d57f8d2587
manfwh <841533182@qq.com>
2026-08-27 10:25:15 +0800

    Wire Amplify Authenticator into the todo page and use owner-based data auth (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,37 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
+import "./../app/app.css";
 
 const client = generateClient<Schema>();
 
-export default function App() {
+function TodoList({
+  user,
+  signOut,
+}: {
+  user?: { signInDetails?: { loginId?: string } };
+  signOut?: () => void;
+}) {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
+  useEffect(() => {
+    const sub = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-  }
-
-  useEffect(() => {
-    listTodos();
+    return () => sub.unsubscribe();
   }, []);
 
   function createTodo() {
     client.models.Todo.create({
-      content: window.prompt("Todo content"),
+      content: window.prompt("Todo content") ?? "",
     });
   }
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -41,12 +41,16 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+        <button onClick={signOut}>Sign out</button>
       </div>
     </main>
+  );
+}
+
+export default function App() {
+  return (
+    <Authenticator loginMechanisms={["email"]}>
+      {({ signOut, user }) => <TodoList user={user} signOut={signOut} />}
+    </Authenticator>
   );
 }
